708feaf0998ed359572478b316b93682b741403a
Paul Lockyer <plockyer@googlemail.com>
2021-10-06 18:04:08 +0100

    Testing requests via form submissions

--- flask_basics/routes.py
+++ flask_basics/routes.py
@@ -2,9 +2,34 @@
 
 # Imports
 from flask_basics import app
-from flask import render_template
+from flask import render_template, request
 
 # Routing
 @app.route("/")
 def index():
     return render_template("index.html", title="Brintey Bitches!!!")
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+
+    if request.method == 'POST':
+        username = request.form.get('username')
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        return '''
+                <h1>Submitted Form Data</h1>
+                <p>Username: {}</p><br>
+                <p>Email: {}</p><br>
+                <p>Password: {}</p><br>
+               '''.format(username, email, password)
+
+    return '''
+            <form method="POST">
+            <div><label>Username: </label><input type="text" name="username"></div>
+            <div><label>Email: </label><input type="email" name="email"></div>
+            <div><label>Password: </label><input type="password" name="password"></div>
+            <div><label>Confirm Password: </label><input type="password" name="confirm-password"></div>
+            <div><input type="submit" value="Submit">
+            </form>
+           '''
